d6465dcf5a8db12080b9a0ded1bab99548c7395a
alex18matei <alex_matei94@yahoo.com>
2016-05-11 01:33:51 +0300

    preluarea continutului paragrafelor dintr-un document xml

---
+++ StringMatching/src/parser/ParserXMLFile.java
@@ -0,0 +1,55 @@
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
+public class ParserXMLFile {
+
+	private List<String> paragraphs;
+
+	ParserXMLFile (String fileName){
+		paragraphs = new ArrayList<String>();
+		this.init(fileName);
+	}
+
+	private void init(String fileName) {
+		try {	
+			File inputFile = new File(fileName);
+			
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+
+			NodeList nList = doc.getElementsByTagName("p");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				
+				Node nNode = nList.item(temp);
+				paragraphs.add(nNode.getTextContent());
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @return the paragraphs
+	 */
+	public List<String> getParagraphs() {
+		return paragraphs;
+	}
+
+	/**
+	 * @param paragraphs the paragraphs to set
+	 */
+	public void setParagraphs(List<String> paragraphs) {
+		this.paragraphs = paragraphs;
+	}
+}
---
+++ StringMatching/src/parser/ParserXMLString.java
@@ -0,0 +1,53 @@
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.w3c.dom.Node;
+
+public class ParserXMLString {
+
+	private List<String> paragraphs;
+
+	ParserXMLString (String xmlString){
+		paragraphs = new ArrayList<String>();
+		this.init(xmlString);
+	}
+
+	private void init(String xmlString) {
+		try {	
+			
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(new InputSource( new StringReader( xmlString )) );
+			doc.getDocumentElement().normalize();
+
+			NodeList nList = doc.getElementsByTagName("p");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				
+				Node nNode = nList.item(temp);
+				paragraphs.add(nNode.getTextContent());
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @return the paragraphs
+	 */
+	public List<String> getParagraphs() {
+		return paragraphs;
+	}
+
+	/**
+	 * @param paragraphs the paragraphs to set
+	 */
+	public void setParagraphs(List<String> paragraphs) {
+		this.paragraphs = paragraphs;
+	}
+}
