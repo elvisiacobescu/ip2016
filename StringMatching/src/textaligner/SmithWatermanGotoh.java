cf6825a423adae24d348f6924efaaf0895b7fcfb
Cezar Elnazli <cezar.elnazli2@gmail.com>
2016-05-15 00:19:17 +0300

    Update SmithWaterman implementation

--- StringMatching/src/textaligner/SmithWatermanGotoh.java
+++ StringMatching/src/textaligner/SmithWatermanGotoh.java
@@ -3,13 +3,12 @@ package textaligner;
 import jaligner.*;
 import jaligner.matrix.*;
 
-public class SmithWatermanGotoh implements TextAligner {
+public final class SmithWatermanGotoh implements TextAligner {
 
     private String firstText;
     private String secondText;
     private Sequence firstSequence;
     private Sequence secondSequence;
-    private float score;
     private int similarity;
     private int firstStart;
     private int secondStart;
@@ -38,10 +37,6 @@ public class SmithWatermanGotoh implements TextAligner {
         return secondText;
     }
 
-    public float getScore() {
-        return score;
-    }
-
     public int getSimilarity() {
         return similarity;
     }
@@ -58,18 +53,13 @@ public class SmithWatermanGotoh implements TextAligner {
         firstSequence = new Sequence(firstText);
         secondSequence = new Sequence(secondText);
 
-        try {
-            Alignment alignment =
-                SmithWatermanGotoh.align(firstSequence,
-                        secondSequence,
-                        MatrixLoader.load("BLOSUM70"), 1.0f, 0.0f);
+        Matrix matrix = jaligner.matrix.MatrixGenerator.generate(2, -1);
+        Alignment alignment =
+            jaligner.SmithWatermanGotoh.align(firstSequence,
+                    secondSequence, matrix, 1.0f, 0.0f);
 
-            score = alignment.calculateScore();
-            similarity = alignment.getSimilarity();
-            firstStart = alignment.getStart1();
-            secondStart = alignment.getStart2();
-        } catch (MatrixLoaderException err) {
-            System.err.println(err);
-        }
+        //similarity = alignment.getSimilarity();
+        firstStart = alignment.getStart1();
+        secondStart = alignment.getStart2();
     }
 }
