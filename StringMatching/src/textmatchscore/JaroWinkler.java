fd4a23b1789986ba2d596b8de09e663d0d1036a8
Cezar <cezar.elnazli2@gmail.com>
2016-05-15 17:41:16 +0300

    Replace Jaro-Winkler implementation

--- StringMatching/src/textmatchscore/JaroWinkler.java
+++ StringMatching/src/textmatchscore/JaroWinkler.java
@@ -1,12 +1,29 @@
+/**
+ *
+ * Adapted from: https://github.com/tdebatty/java-string-similarity
+ *
+ */
 package textmatchscore;
 
+import java.util.Arrays;
+
+/**
+ * The Jaro–Winkler distance metric is designed and best suited for short
+ * strings such as person names, and to detect typos; it is (roughly) a
+ * variation of Damerau-Levenshtein, where the substitution of 2 close
+ * characters is considered less important then the substitution of 2 characters
+ * that a far from each other.
+ * Jaro-Winkler was developed in the area of record linkage (duplicate
+ * detection) (Winkler, 1990). It returns a value in the interval [0.0, 1.0].
+ * The distance is computed as 1 - Jaro-Winkler similarity.
+ * @author Thibault Debatty
+ */
 public class JaroWinkler implements TextMatchScore {
+
     private String firstString;
     private String secondString;
 
-    private String theMatchA = "";
-    private String theMatchB = "";
-    private int mRange = -1;
+    private double threshold = 0.7;
 
     public JaroWinkler(String firstString, String secondString) {
         this.firstString = firstString;
@@ -14,102 +31,83 @@ public class JaroWinkler implements TextMatchScore {
     }
 
     public double getScore() {
-        return getSimilarity();
+        return 1.0 - similarity();
     }
 
-    public double getSimilarity() {
-        mRange = Math.max(firstString.length(), secondString.length()) / 2 - 1;
+    private final void setThreshold(double threshold) {
+        this.threshold = threshold;
+    }
 
-        int m = getMatch();
-        int t = 0;
+    private double getThreshold() {
+        return threshold;
+    }
 
-        if (getMismatch(secondString, firstString) > 0) {
-            t = getMismatch(firstString, secondString) / getMismatch(secondString, firstString);
+    private double similarity() {
+        int[] mtp = matches();
+        float m = mtp[0];
+        if (m == 0) {
+            return 0f;
         }
-
-        int l1 = firstString.length();
-        int l2 = secondString.length();
-
-        double f = 0.3333;
-        double mt = (double) (m - t) / m;
-        double jw = f * ((double) m / l1 + (double) m / l2 + mt);
-        double res = jw + getCommonPrefix(firstString, secondString) * (0.1 * (1.0 - jw));
-
-        return res;
+        float j = ((m / firstString.length() + m / secondString.length() + (m - mtp[1]) / m)) / 3;
+        float jw = j < getThreshold() ? j : j + Math.min(0.1f, 1f / mtp[3]) * mtp[2]
+                * (1 - j);
+        return jw;
     }
 
-    private int getMatch() {
-
-        theMatchA = "";
-        theMatchB = "";
-
+    private int[] matches() {
+        String max, min;
+        if (firstString.length() > secondString.length()) {
+            max = firstString;
+            min = secondString;
+        } else {
+            max = secondString;
+            min = firstString;
+        }
+        int range = Math.max(max.length() / 2 - 1, 0);
+        int[] matchIndexes = new int[min.length()];
+        Arrays.fill(matchIndexes, -1);
+        boolean[] matchFlags = new boolean[max.length()];
         int matches = 0;
-
-        for (int i = 0; i < firstString.length(); i++) {
-
-            //Look backward
-            int counter = 0;
-            while(counter <= mRange && i >= 0 && counter <= i) {
-                if (firstString.charAt(i) == secondString.charAt(i - counter)) {
+        for (int mi = 0; mi < min.length(); mi++) {
+            char c1 = min.charAt(mi);
+            for (int xi = Math.max(mi - range, 0),
+                    xn = Math.min(mi + range + 1, max.length()); xi < xn; xi++) {
+                if (!matchFlags[xi] && c1 == max.charAt(xi)) {
+                    matchIndexes[mi] = xi;
+                    matchFlags[xi] = true;
                     matches++;
-                    theMatchA = theMatchA + firstString.charAt(i);
-                    theMatchB = theMatchB + secondString.charAt(i);
+                    break;
                 }
-                counter++;
-            }
-
-            //Look forward
-            counter = 1;
-            while(counter <= mRange && i < secondString.length() && counter + i < secondString.length()) {
-                if (firstString.charAt(i) == secondString.charAt(i + counter)) {
-                    matches++;
-                    theMatchA = theMatchA + firstString.charAt(i);
-                    theMatchB = theMatchB + secondString.charAt(i);
-                }
-                counter++;
             }
         }
-        return matches;
-    }
-
-    private int getMismatch(String s1, String s2)
-    {
-        int transPositions = 0;
-
-        for (int i = 0; i < theMatchA.length(); i++)
-        {
-            //Look Backward
-            int counter = 0;
-            while(counter <= mRange && i >= 0 && counter <= i)
-            {
-                if (theMatchA.charAt(i) == theMatchB.charAt(i - counter) && counter > 0)
-                {
-                    transPositions++;
-                }
-                counter++;
-            }
-
-            //Look forward
-            counter = 1;
-            while(counter <= mRange && i < theMatchB.length() && (counter + i) < theMatchB.length())
-            {
-                if (theMatchA.charAt(i) == theMatchB.charAt(i + counter) && counter > 0)
-                {
-                    transPositions++;
-                }
-                counter++;
+        char[] mfirstString = new char[matches];
+        char[] msecondString = new char[matches];
+        for (int i = 0, si = 0; i < min.length(); i++) {
+            if (matchIndexes[i] != -1) {
+                mfirstString[si] = min.charAt(i);
+                si++;
             }
         }
-        return transPositions;
-    }
-
-    private int getCommonPrefix(String firstString, String secondString)
-    {
-        int cp = 0;
-        for (int i = 0; i < 4; i++)
-        {
-            if (firstString.charAt(i) == secondString.charAt(i)) cp++;
+        for (int i = 0, si = 0; i < max.length(); i++) {
+            if (matchFlags[i]) {
+                msecondString[si] = max.charAt(i);
+                si++;
+            }
         }
-        return cp;
+        int transpositions = 0;
+        for (int mi = 0; mi < mfirstString.length; mi++) {
+            if (mfirstString[mi] != msecondString[mi]) {
+                transpositions++;
+            }
+        }
+        int prefix = 0;
+        for (int mi = 0; mi < min.length(); mi++) {
+            if (firstString.charAt(mi) == secondString.charAt(mi)) {
+                prefix++;
+            } else {
+                break;
+            }
+        }
+        return new int[]{matches, transpositions / 2, prefix, max.length()};
     }
 }
