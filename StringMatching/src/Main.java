049e4619d226d6e53963b77862464bf9ff07a198
Cezar <cezar.elnazli2@gmail.com>
2016-05-15 18:23:48 +0300

    Integrate score calculator in Main

--- StringMatching/src/Main.java
+++ StringMatching/src/Main.java
@@ -5,48 +5,108 @@ import textmatchscore.*;
 
 public class Main {
 
+    private static ParserXMLFile firstFile;
+    private static ParserXMLFile secondFile;
+    private static TextNormalizer normalizer;
+
+    private static AbstractFactory textAlignerFactory;
+    private static AbstractFactory alignmentScoreFactory;
+
+    private static void initParsers() throws AlignmentParserException {
+        firstFile = new ParserXMLFile("..\\tests\\samples\\slov1a.txt");
+        secondFile = new ParserXMLFile("..\\tests\\samples\\slov1b.txt");
+        normalizer = new TextNormalizer();
+    }
+
+    private static void initFactories() throws AlignmentFactoryException {
+        textAlignerFactory = FactoryProducer.getFactory("Aligner");
+        alignmentScoreFactory = FactoryProducer.getFactory("Score");
+    }
+
     public static void main(String[] args) {
         try {
-            ParserXMLFile firstFile =
-                new ParserXMLFile("..\\tests\\samples\\slov1a.txt");
+            initParsers();
+            initFactories();
+        } catch (AlignmentParserException | AlignmentFactoryException err) {
+            System.err.println(err);
+            System.exit(1);
+        }
 
-            ParserXMLFile secondFile =
-                new ParserXMLFile("..\\tests\\samples\\slov1b.txt");
+        for (Paragraph firstParagraph : firstFile.getParagraphs()) {
 
-            TextNormalizer normalizer = new TextNormalizer();
+            normalizer.setInitialText(firstParagraph.getParagraphContent());
+            normalizer.normalize();
+            firstParagraph.setParagraphContent(normalizer.getFinalText());
 
-            AbstractFactory textAlignerFactory =
-                FactoryProducer.getFactory("Aligner");
+            for (Paragraph secondParagraph : secondFile.getParagraphs()) {
 
-            AbstractFactory alignmentScoreFactory =
-                FactoryProducer.getFactory("Score");
-
-            for (Paragraph firstParagraph : firstFile.getParagraphs()) {
-
-                normalizer.setInitialText(firstParagraph.getParagraphContent());
+                normalizer.setInitialText(secondParagraph.getParagraphContent());
                 normalizer.normalize();
-                firstParagraph.setParagraphContent(normalizer.getFinalText());
+                secondParagraph.setParagraphContent(normalizer.getFinalText());
 
-                for (Paragraph secondParagraph : secondFile.getParagraphs()) {
+                for (String algorithm : textAlignerFactory.getAlgorithms()) {
 
-                    normalizer.setInitialText(secondParagraph.getParagraphContent());
-                    normalizer.normalize();
-                    secondParagraph.setParagraphContent(normalizer.getFinalText());
-
-                    for (String algorithm : textAlignerFactory.getAlgorithms()) {
-
-                        TextAligner textAligner =
+                    TextAligner textAligner;
+                    try {
+                        textAligner =
                             textAlignerFactory.getTextAligner(
                                     algorithm,
                                     firstParagraph.getParagraphContent(),
                                     secondParagraph.getParagraphContent());
 
                         textAligner.align();
+                    } catch (AlignmentFactoryException err) {
+                        System.err.println(err);
+                        continue;
+                    }
+
+                    String firstAlignment =
+                        firstParagraph.getParagraphContent().substring(
+                                textAligner.getFirstStart());
+
+                    String secondAlignment =
+                        secondParagraph.getParagraphContent().substring(
+                                textAligner.getSecondStart());
+
+                    for (String calculator : alignmentScoreFactory.getAlgorithms()) {
+
+                        TextMatchScore scoreCalculator;
+                        try {
+                            scoreCalculator =
+                                alignmentScoreFactory.getTextMatchScore(
+                                        calculator,
+                                        firstAlignment,
+                                        secondAlignment);
+                        } catch (AlignmentFactoryException err) {
+                            System.err.println(err);
+                            continue;
+                        }
+
+                        try {
+                            double score = scoreCalculator.getScore();
+
+                            System.out.print("First Paragraph ID: ");
+                            System.out.println(firstParagraph.getParagraphNum());
+
+                            System.out.print("Second Paragraph ID: ");
+                            System.out.println(secondParagraph.getParagraphNum());
+
+                            System.out.print("Aligner: ");
+                            System.out.println(algorithm);
+
+                            System.out.print("Calculator: ");
+                            System.out.println(calculator);
+
+                            System.out.print("Score: ");
+                            System.out.println(score);
+
+                            System.out.println("");
+                        } catch (AlignmentScoreException err) {
+                            System.err.println(err);
+                        }
                     }
                 }
             }
-        } catch (AlignmentParserException | AlignmentFactoryException err) {
-            System.err.println(err);
         }
     }
 }
