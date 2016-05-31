d6f576f8efdc7d6e6b50410700e01542d21d1645
alex18matei <alex_matei94@yahoo.com>
2016-05-31 18:10:41 +0300

    update Main
    update Main ( write the result to a file)

--- StringMatching/src/Main.java
+++ StringMatching/src/Main.java
@@ -1,112 +1,52 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 import factory.*;
 import parser.*;
-import textaligner.*;
-import textmatchscore.*;
 
 public class Main {
 
-    private static ParserXMLFile firstFile;
-    private static ParserXMLFile secondFile;
-    private static TextNormalizer normalizer;
-
-    private static AbstractFactory textAlignerFactory;
-    private static AbstractFactory alignmentScoreFactory;
-
-    private static void initParsers() throws AlignmentParserException {
-        firstFile = new ParserXMLFile("..\\tests\\samples\\slov1a.txt");
-        secondFile = new ParserXMLFile("..\\tests\\samples\\slov1b.txt");
-        normalizer = new TextNormalizer();
-    }
-
-    private static void initFactories() throws AlignmentFactoryException {
-        textAlignerFactory = FactoryProducer.getFactory("Aligner");
-        alignmentScoreFactory = FactoryProducer.getFactory("Score");
-    }
-
     public static void main(String[] args) {
         try {
-            initParsers();
-            initFactories();
-        } catch (AlignmentParserException | AlignmentFactoryException err) {
-            System.err.println(err);
-            System.exit(1);
-        }
-
-        for (Paragraph firstParagraph : firstFile.getParagraphs()) {
-
-            normalizer.setInitialText(firstParagraph.getParagraphContent());
-            normalizer.normalize();
-            firstParagraph.setParagraphContent(normalizer.getFinalText());
-
-            for (Paragraph secondParagraph : secondFile.getParagraphs()) {
-
-                normalizer.setInitialText(secondParagraph.getParagraphContent());
-                normalizer.normalize();
-                secondParagraph.setParagraphContent(normalizer.getFinalText());
-
-                for (String algorithm : textAlignerFactory.getAlgorithms()) {
-
-                    TextAligner textAligner;
-                    try {
-                        textAligner =
-                            textAlignerFactory.getTextAligner(
-                                    algorithm,
-                                    firstParagraph.getParagraphContent(),
-                                    secondParagraph.getParagraphContent());
-
-                        textAligner.align();
-                    } catch (AlignmentFactoryException err) {
-                        System.err.println(err);
-                        continue;
-                    }
-
-                    String firstAlignment =
-                        firstParagraph.getParagraphContent().substring(
-                                textAligner.getFirstStart());
-
-                    String secondAlignment =
-                        secondParagraph.getParagraphContent().substring(
-                                textAligner.getSecondStart());
-
-                    for (String calculator : alignmentScoreFactory.getAlgorithms()) {
-
-                        TextMatchScore scoreCalculator;
-                        try {
-                            scoreCalculator =
-                                alignmentScoreFactory.getTextMatchScore(
-                                        calculator,
-                                        firstAlignment,
-                                        secondAlignment);
-                        } catch (AlignmentFactoryException err) {
-                            System.err.println(err);
-                            continue;
-                        }
-
-                        try {
-                            double score = scoreCalculator.getScore();
-
-                            System.out.print("First Paragraph ID: ");
-                            System.out.println(firstParagraph.getParagraphNum());
-
-                            System.out.print("Second Paragraph ID: ");
-                            System.out.println(secondParagraph.getParagraphNum());
-
-                            System.out.print("Aligner: ");
-                            System.out.println(algorithm);
-
-                            System.out.print("Calculator: ");
-                            System.out.println(calculator);
-
-                            System.out.print("Score: ");
-                            System.out.println(score);
-
-                            System.out.println("");
-                        } catch (AlignmentScoreException err) {
-                            System.err.println(err);
-                        }
-                    }
-                }
-            }
-        }
+        	String filePathOne = "../tests/samples/slov1a.txt";
+        	String filePathTwo = "../tests/samples/slov1b.txt";
+			List<AlignmentPair> list = StringMatching.getParagraphPairs(filePathOne,filePathTwo);
+			for( AlignmentPair pair : list){
+				System.out.println(pair);
+			}
+			save(filePathOne,filePathTwo,list);
+		} catch (AlignmentParserException|AlignmentFactoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
+    
+    public static void save(String filePathOne, String filePathTwo, List<AlignmentPair> list){
+    	
+		BufferedWriter br = null;
+		try{
+			
+			FileWriter fileWriter = new FileWriter("file.txt");
+			br = new BufferedWriter(fileWriter);
+			br.write(filePathOne+'\n');
+			br.write(filePathTwo+'\n');
+			
+			for(int i = 0; i < list.size(); i++){
+				
+				br.write(list.get(i) + "\n");
+			}
+		}
+		catch( IOException e ){
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
